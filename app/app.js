b772ff1180a6d05dcd1704ef2c21545360b60018
sandy <sandeepdesu@sandys-MacBook-Air.local>
2018-12-20 22:25:59 +0530

    post service

--- app/app.js
+++ app/app.js
@@ -19,8 +19,15 @@ app.controller('first', ['$scope', 'firstService', 'firstFactoryMethod', '$state
     $scope.name = "async call resquest example";
     $scope.subjects = [];
     $scope.jobs = [];
-    firstService.getRequest().then(function (data) {
+    firstService.get().then(function (data) {
         $scope.subjects = data;
+    });
+
+    firstService.post({
+        name:'angular',
+        description:'Its a framework'
+    }).then(function (data) {
+        $scope.info = data;
     });
 
     firstFactoryMethod.show().$promise.then(function (res) {
@@ -39,12 +46,12 @@ app.controller('first', ['$scope', 'firstService', 'firstFactoryMethod', '$state
     $scope.changeRoute = function () {
         //$state.go('subjects');
     }
-}]).factory('firstService', ['$q', '$http', function ($q, $http) {
-    var get = function () {
+}]).service('firstService', ['$q', '$http', function ($q, $http) {
+    this.get = function () {
         var d = $q.defer();
         $http({
             method: 'GET',
-            url: 'https://api.talentscreen.io/v1/subjects',
+            url: 'http://localhost:8000/v1/subjects',
             headers: {
                 'content-type': 'application/json'
             }
@@ -54,8 +61,19 @@ app.controller('first', ['$scope', 'firstService', 'firstFactoryMethod', '$state
         return d.promise;
     }
 
-    return {
-        getRequest: get
+    this.post = function(info){
+        var d = $q.defer();
+        $http({
+            method: 'POST',
+            url: 'http://localhost:8000/v1/subjects',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body:JSON.stringify(info)
+        }).then(function (result) {
+            d.resolve(result.data);
+        })
+        return d.promise;
     }
 }]).factory('firstFactoryMethod', ['$resource', function ($resource) {
     return $resource('https://api.qa.talentscreen.io/v1/jobs?authentication=false&limit=10', {}, {
